ef504f68e0eb1d42cdba1598e457554f406727c2
murai728 <98868550+murai728@users.noreply.github.com>
2023-10-20 15:47:46 +0900

    1547

--- Form/F_商品管理_抽出.Designer.cs
+++ Form/F_商品管理_抽出.Designer.cs
@@ -28,12 +28,189 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            売上区分コード = new ComboBox();
+            label9 = new Label();
+            label7 = new Label();
+            シリーズ名 = new TextBox();
+            基本型式名 = new TextBox();
+            label5 = new Label();
+            label1 = new Label();
+            更新日開始 = new TextBox();
+            label2 = new Label();
+            更新日終了 = new TextBox();
+            SuspendLayout();
+            // 
+            // 売上区分コード
+            // 
+            売上区分コード.DisplayMember = "更新者名";
+            売上区分コード.FormattingEnabled = true;
+            売上区分コード.Location = new Point(116, 132);
+            売上区分コード.Margin = new Padding(4, 3, 4, 3);
+            売上区分コード.Name = "売上区分コード";
+            売上区分コード.Size = new Size(160, 23);
+            売上区分コード.TabIndex = 198;
+            売上区分コード.ValueMember = "売上区分コード";
+            // 
+            // label9
+            // 
+            label9.AllowDrop = true;
+            label9.AutoEllipsis = true;
+            label9.Font = new Font("BIZ UDゴシック", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            label9.ForeColor = SystemColors.ActiveCaptionText;
+            label9.ImageAlign = ContentAlignment.MiddleLeft;
+            label9.Location = new Point(27, 132);
+            label9.Margin = new Padding(0);
+            label9.Name = "label9";
+            label9.Size = new Size(85, 17);
+            label9.TabIndex = 197;
+            label9.Text = "更新者名(N)";
+            label9.TextAlign = ContentAlignment.MiddleLeft;
+            // 
+            // label7
+            // 
+            label7.AllowDrop = true;
+            label7.AutoEllipsis = true;
+            label7.Font = new Font("BIZ UDゴシック", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            label7.ForeColor = SystemColors.ActiveCaptionText;
+            label7.ImageAlign = ContentAlignment.MiddleLeft;
+            label7.Location = new Point(24, 58);
+            label7.Margin = new Padding(0);
+            label7.Name = "label7";
+            label7.Size = new Size(85, 17);
+            label7.TabIndex = 196;
+            label7.Text = "シリーズ名(S)";
+            label7.TextAlign = ContentAlignment.MiddleLeft;
+            // 
+            // シリーズ名
+            // 
+            シリーズ名.BackColor = Color.White;
+            シリーズ名.Enabled = false;
+            シリーズ名.Font = new Font("BIZ UDゴシック", 9.75F, FontStyle.Regular, GraphicsUnit.Point);
+            シリーズ名.Location = new Point(116, 57);
+            シリーズ名.Margin = new Padding(3, 2, 3, 2);
+            シリーズ名.Multiline = true;
+            シリーズ名.Name = "シリーズ名";
+            シリーズ名.ReadOnly = true;
+            シリーズ名.Size = new Size(413, 23);
+            シリーズ名.TabIndex = 195;
+            シリーズ名.Visible = false;
+            // 
+            // 基本型式名
+            // 
+            基本型式名.BackColor = Color.White;
+            基本型式名.Font = new Font("BIZ UDゴシック", 9.75F, FontStyle.Regular, GraphicsUnit.Point);
+            基本型式名.Location = new Point(116, 23);
+            基本型式名.Margin = new Padding(3, 2, 3, 2);
+            基本型式名.Multiline = true;
+            基本型式名.Name = "基本型式名";
+            基本型式名.Size = new Size(413, 23);
+            基本型式名.TabIndex = 194;
+            // 
+            // label5
+            // 
+            label5.AllowDrop = true;
+            label5.AutoEllipsis = true;
+            label5.Font = new Font("BIZ UDゴシック", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            label5.ForeColor = SystemColors.ActiveCaptionText;
+            label5.ImageAlign = ContentAlignment.MiddleLeft;
+            label5.Location = new Point(27, 23);
+            label5.Margin = new Padding(0);
+            label5.Name = "label5";
+            label5.Size = new Size(87, 17);
+            label5.TabIndex = 193;
+            label5.Text = "基本型式名(N)";
+            label5.TextAlign = ContentAlignment.MiddleLeft;
+            // 
+            // label1
+            // 
+            label1.AllowDrop = true;
+            label1.AutoEllipsis = true;
+            label1.Font = new Font("BIZ UDゴシック", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.ForeColor = SystemColors.ActiveCaptionText;
+            label1.ImageAlign = ContentAlignment.MiddleLeft;
+            label1.Location = new Point(24, 95);
+            label1.Margin = new Padding(0);
+            label1.Name = "label1";
+            label1.Size = new Size(85, 17);
+            label1.TabIndex = 200;
+            label1.Text = "更新日(U)";
+            label1.TextAlign = ContentAlignment.MiddleLeft;
+            // 
+            // 更新日開始
+            // 
+            更新日開始.BackColor = Color.White;
+            更新日開始.Enabled = false;
+            更新日開始.Font = new Font("BIZ UDゴシック", 9.75F, FontStyle.Regular, GraphicsUnit.Point);
+            更新日開始.Location = new Point(116, 94);
+            更新日開始.Margin = new Padding(3, 2, 3, 2);
+            更新日開始.Multiline = true;
+            更新日開始.Name = "更新日開始";
+            更新日開始.ReadOnly = true;
+            更新日開始.Size = new Size(160, 23);
+            更新日開始.TabIndex = 199;
+            更新日開始.Visible = false;
+            // 
+            // label2
+            // 
+            label2.AllowDrop = true;
+            label2.AutoEllipsis = true;
+            label2.Font = new Font("BIZ UDゴシック", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            label2.ForeColor = SystemColors.ActiveCaptionText;
+            label2.ImageAlign = ContentAlignment.MiddleLeft;
+            label2.Location = new Point(311, 96);
+            label2.Margin = new Padding(0);
+            label2.Name = "label2";
+            label2.Size = new Size(27, 16);
+            label2.TabIndex = 202;
+            label2.Text = "～";
+            label2.TextAlign = ContentAlignment.MiddleLeft;
+            // 
+            // 更新日終了
+            // 
+            更新日終了.BackColor = Color.White;
+            更新日終了.Enabled = false;
+            更新日終了.Font = new Font("BIZ UDゴシック", 9.75F, FontStyle.Regular, GraphicsUnit.Point);
+            更新日終了.Location = new Point(369, 95);
+            更新日終了.Margin = new Padding(3, 2, 3, 2);
+            更新日終了.Multiline = true;
+            更新日終了.Name = "更新日終了";
+            更新日終了.ReadOnly = true;
+            更新日終了.Size = new Size(160, 23);
+            更新日終了.TabIndex = 201;
+            更新日終了.Visible = false;
+            // 
+            // F_商品管理_抽出
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(label2);
+            Controls.Add(更新日終了);
+            Controls.Add(label1);
+            Controls.Add(更新日開始);
+            Controls.Add(売上区分コード);
+            Controls.Add(label9);
+            Controls.Add(label7);
+            Controls.Add(シリーズ名);
+            Controls.Add(基本型式名);
+            Controls.Add(label5);
+            Name = "F_商品管理_抽出";
             Text = "F_商品管理_抽出";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private ComboBox 売上区分コード;
+        private Label label9;
+        private Label label7;
+        private TextBox シリーズ名;
+        private TextBox 基本型式名;
+        private Label label5;
+        private Label label1;
+        private TextBox 更新日開始;
+        private Label label2;
+        private TextBox 更新日終了;
     }
 }
