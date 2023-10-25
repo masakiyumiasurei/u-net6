18842aedf47f3ff664fdc7daaa73c48cf325f729
Ryota Tanaka <tanaka.r@hbm-web.co.jp>
2023-10-25 10:34:51 +0900

    別フォームを開いて値取得

---
+++ Form/F_カレンダー.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace u_net
+{
+    partial class F_カレンダー
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+        private TextBox 日付;
+        private Button btnOK;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        private void InitializeComponent()
+        {
+            this.日付 = new System.Windows.Forms.TextBox();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // 日付
+            // 
+            this.日付.Location = new System.Drawing.Point(87, 134);
+            this.日付.Name = "日付";
+            this.日付.Size = new System.Drawing.Size(123, 23);
+            this.日付.TabIndex = 0;
+            // 
+            // btnOK
+            // 
+            this.btnOK.Location = new System.Drawing.Point(29, 48);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(88, 49);
+            this.btnOK.TabIndex = 1;
+            this.btnOK.Text = "button1";
+            this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // MidForm
+            // 
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.btnOK);
+            this.Controls.Add(this.日付);
+            this.Name = "MidForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+    }
+}
---
+++ Form/F_カレンダー.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Data.SqlClient;
+using System.Data.OleDb;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+using u_net.Public;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json.Linq;
+using System.Drawing.Imaging;
+using System.Drawing.Printing;
+
+
+namespace u_net
+{
+    public partial class F_カレンダー : Form
+    {
+        public string SelectedDate { get; private set; }
+
+        public F_カレンダー()
+        {
+
+            this.Text = "カレンダー";       // ウィンドウタイトルを設定
+            this.MaximizeBox = false;  // 最大化ボタンを無効化
+            this.MinimizeBox = false; //最小化ボタンを無効化
+
+            InitializeComponent();
+        }
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            // 日付選択フォームから選択した日付を取得
+            SelectedDate = this.日付.Text;
+
+            // ダイアログをOKで閉じる
+            DialogResult = DialogResult.OK;
+            Close();
+        }
+    }
+}
