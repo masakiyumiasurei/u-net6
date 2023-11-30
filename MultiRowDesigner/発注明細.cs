a2b59bd83588f5c1c6689545f2c344eb32be8cda
Kinoshita Daichi <kinoshita.d@hbm-web.co.jp>
2023-11-30 16:27:42 +0900

    発注明細テンプレート作成

---
+++ MultiRowDesigner/発注明細.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using GrapeCity.Win.MultiRow;
+
+namespace MultiRowDesigner
+{
+    public partial class 発注明細 : UserControl
+    {
+        public GcMultiRow Detail
+        {
+            get
+            {
+                return gcMultiRow1;
+            }
+        }
+
+        public 発注明細()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
