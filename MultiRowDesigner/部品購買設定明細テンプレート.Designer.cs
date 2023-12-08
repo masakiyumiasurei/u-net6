a2f6c8cabe83026f2a788428d7a4e3819e060948
Kinoshita Daichi <kinoshita.d@hbm-web.co.jp>
2023-12-08 17:22:25 +0900

    F_部品購買設定明細作成

---
+++ MultiRowDesigner/部品購買設定明細テンプレート.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace MultiRowDesigner
+{          
+    [System.ComponentModel.ToolboxItem(true)]
+    partial class 部品購買設定明細テンプレート
+    {
+        /// <summary> 
+        /// 必要なデザイナ変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+		/// <summary> 
+		/// 使用中のリソースをすべてクリーンアップします。
+		/// </summary>
+		/// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region MultiRow Template Designer generated code
+
+		/// <summary> 
+		/// デザイナ サポートに必要なメソッドです。このメソッドの内容を
+		/// コード エディタで変更しないでください。
+		/// </summary>
+        private void InitializeComponent()
+        {
+            this.columnHeaderSection1 = new GrapeCity.Win.MultiRow.ColumnHeaderSection();
+            // 
+            // Row
+            // 
+            this.Row.Height = 72;
+            this.Row.Width = 806;
+            // 
+            // columnHeaderSection1
+            // 
+            this.columnHeaderSection1.Height = 45;
+            this.columnHeaderSection1.Name = "columnHeaderSection1";
+            this.columnHeaderSection1.Width = 806;
+            // 
+            // 部品購買設定明細テンプレート
+            // 
+            this.ColumnHeaders.AddRange(new GrapeCity.Win.MultiRow.ColumnHeaderSection[] {
+            this.columnHeaderSection1});
+            this.Height = 117;
+            this.Width = 806;
+
+        }
+        
+
+        #endregion
+
+        private GrapeCity.Win.MultiRow.ColumnHeaderSection columnHeaderSection1;
+        
+    }
+}
