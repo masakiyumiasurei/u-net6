9223d3eee8edf65c0dc7cdef160864ddf7942728
Kinoshita Daichi <kinoshita.d@hbm-web.co.jp>
2023-12-14 10:32:42 +0900

    F_ユニット明細参照作成

---
+++ MultiRowDesigner/ユニット明細参照テンプレート.Designer.cs
@@ -0,0 +1,269 @@
+﻿namespace MultiRowDesigner
+{          
+    [System.ComponentModel.ToolboxItem(true)]
+    partial class ユニット明細参照テンプレート
+    {
+        /// <summary> 
+        /// 必要なデザイナ変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+		/// <summary> 
+		/// 使用中のリソースをすべてクリーンアップします。
+		/// </summary>
+		/// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region MultiRow Template Designer generated code
+
+		/// <summary> 
+		/// デザイナ サポートに必要なメソッドです。このメソッドの内容を
+		/// コード エディタで変更しないでください。
+		/// </summary>
+        private void InitializeComponent()
+        {
+            this.columnHeaderSection1 = new GrapeCity.Win.MultiRow.ColumnHeaderSection();
+            this.columnHeaderCell1 = new GrapeCity.Win.MultiRow.ColumnHeaderCell();
+            this.columnHeaderCell2 = new GrapeCity.Win.MultiRow.ColumnHeaderCell();
+            this.columnHeaderCell3 = new GrapeCity.Win.MultiRow.ColumnHeaderCell();
+            this.columnHeaderCell4 = new GrapeCity.Win.MultiRow.ColumnHeaderCell();
+            this.columnHeaderCell5 = new GrapeCity.Win.MultiRow.ColumnHeaderCell();
+            this.columnHeaderCell6 = new GrapeCity.Win.MultiRow.ColumnHeaderCell();
+            this.columnHeaderCell7 = new GrapeCity.Win.MultiRow.ColumnHeaderCell();
+            this.columnHeaderCell8 = new GrapeCity.Win.MultiRow.ColumnHeaderCell();
+            this.columnHeaderCell9 = new GrapeCity.Win.MultiRow.ColumnHeaderCell();
+            this.columnHeaderCell10 = new GrapeCity.Win.MultiRow.ColumnHeaderCell();
+            this.cornerHeaderCell1 = new GrapeCity.Win.MultiRow.CornerHeaderCell();
+            this.textBoxCell1 = new GrapeCity.Win.MultiRow.TextBoxCell();
+            this.textBoxCell2 = new GrapeCity.Win.MultiRow.TextBoxCell();
+            this.textBoxCell3 = new GrapeCity.Win.MultiRow.TextBoxCell();
+            this.textBoxCell4 = new GrapeCity.Win.MultiRow.TextBoxCell();
+            this.textBoxCell5 = new GrapeCity.Win.MultiRow.TextBoxCell();
+            this.textBoxCell6 = new GrapeCity.Win.MultiRow.TextBoxCell();
+            this.textBoxCell7 = new GrapeCity.Win.MultiRow.TextBoxCell();
+            this.textBoxCell8 = new GrapeCity.Win.MultiRow.TextBoxCell();
+            this.textBoxCell9 = new GrapeCity.Win.MultiRow.TextBoxCell();
+            this.textBoxCell10 = new GrapeCity.Win.MultiRow.TextBoxCell();
+            this.rowHeaderCell1 = new GrapeCity.Win.MultiRow.RowHeaderCell();
+            // 
+            // columnHeaderSection1
+            // 
+            this.columnHeaderSection1.Cells.Add(this.columnHeaderCell1);
+            this.columnHeaderSection1.Cells.Add(this.columnHeaderCell2);
+            this.columnHeaderSection1.Cells.Add(this.columnHeaderCell3);
+            this.columnHeaderSection1.Cells.Add(this.columnHeaderCell4);
+            this.columnHeaderSection1.Cells.Add(this.columnHeaderCell5);
+            this.columnHeaderSection1.Cells.Add(this.columnHeaderCell6);
+            this.columnHeaderSection1.Cells.Add(this.columnHeaderCell7);
+            this.columnHeaderSection1.Cells.Add(this.columnHeaderCell8);
+            this.columnHeaderSection1.Cells.Add(this.columnHeaderCell9);
+            this.columnHeaderSection1.Cells.Add(this.columnHeaderCell10);
+            this.columnHeaderSection1.Cells.Add(this.cornerHeaderCell1);
+            this.columnHeaderSection1.Height = 21;
+            this.columnHeaderSection1.Name = "columnHeaderSection1";
+            this.columnHeaderSection1.Width = 840;
+            // 
+            // columnHeaderCell1
+            // 
+            this.columnHeaderCell1.Location = new System.Drawing.Point(40, 0);
+            this.columnHeaderCell1.Name = "columnHeaderCell1";
+            this.columnHeaderCell1.Size = new System.Drawing.Size(80, 21);
+            this.columnHeaderCell1.TabIndex = 0;
+            // 
+            // columnHeaderCell2
+            // 
+            this.columnHeaderCell2.Location = new System.Drawing.Point(120, 0);
+            this.columnHeaderCell2.Name = "columnHeaderCell2";
+            this.columnHeaderCell2.Size = new System.Drawing.Size(80, 21);
+            this.columnHeaderCell2.TabIndex = 1;
+            // 
+            // columnHeaderCell3
+            // 
+            this.columnHeaderCell3.Location = new System.Drawing.Point(200, 0);
+            this.columnHeaderCell3.Name = "columnHeaderCell3";
+            this.columnHeaderCell3.Size = new System.Drawing.Size(80, 21);
+            this.columnHeaderCell3.TabIndex = 2;
+            // 
+            // columnHeaderCell4
+            // 
+            this.columnHeaderCell4.Location = new System.Drawing.Point(280, 0);
+            this.columnHeaderCell4.Name = "columnHeaderCell4";
+            this.columnHeaderCell4.Size = new System.Drawing.Size(80, 21);
+            this.columnHeaderCell4.TabIndex = 3;
+            // 
+            // columnHeaderCell5
+            // 
+            this.columnHeaderCell5.Location = new System.Drawing.Point(360, 0);
+            this.columnHeaderCell5.Name = "columnHeaderCell5";
+            this.columnHeaderCell5.Size = new System.Drawing.Size(80, 21);
+            this.columnHeaderCell5.TabIndex = 4;
+            // 
+            // columnHeaderCell6
+            // 
+            this.columnHeaderCell6.Location = new System.Drawing.Point(440, 0);
+            this.columnHeaderCell6.Name = "columnHeaderCell6";
+            this.columnHeaderCell6.Size = new System.Drawing.Size(80, 21);
+            this.columnHeaderCell6.TabIndex = 5;
+            // 
+            // columnHeaderCell7
+            // 
+            this.columnHeaderCell7.Location = new System.Drawing.Point(520, 0);
+            this.columnHeaderCell7.Name = "columnHeaderCell7";
+            this.columnHeaderCell7.Size = new System.Drawing.Size(80, 21);
+            this.columnHeaderCell7.TabIndex = 6;
+            // 
+            // columnHeaderCell8
+            // 
+            this.columnHeaderCell8.Location = new System.Drawing.Point(600, 0);
+            this.columnHeaderCell8.Name = "columnHeaderCell8";
+            this.columnHeaderCell8.Size = new System.Drawing.Size(80, 21);
+            this.columnHeaderCell8.TabIndex = 7;
+            // 
+            // columnHeaderCell9
+            // 
+            this.columnHeaderCell9.Location = new System.Drawing.Point(680, 0);
+            this.columnHeaderCell9.Name = "columnHeaderCell9";
+            this.columnHeaderCell9.Size = new System.Drawing.Size(80, 21);
+            this.columnHeaderCell9.TabIndex = 8;
+            // 
+            // columnHeaderCell10
+            // 
+            this.columnHeaderCell10.Location = new System.Drawing.Point(760, 0);
+            this.columnHeaderCell10.Name = "columnHeaderCell10";
+            this.columnHeaderCell10.Size = new System.Drawing.Size(80, 21);
+            this.columnHeaderCell10.TabIndex = 9;
+            // 
+            // cornerHeaderCell1
+            // 
+            this.cornerHeaderCell1.Location = new System.Drawing.Point(0, 0);
+            this.cornerHeaderCell1.Name = "cornerHeaderCell1";
+            this.cornerHeaderCell1.Size = new System.Drawing.Size(40, 21);
+            this.cornerHeaderCell1.TabIndex = 10;
+            // 
+            // textBoxCell1
+            // 
+            this.textBoxCell1.Location = new System.Drawing.Point(40, 0);
+            this.textBoxCell1.Name = "textBoxCell1";
+            this.textBoxCell1.TabIndex = 0;
+            // 
+            // textBoxCell2
+            // 
+            this.textBoxCell2.Location = new System.Drawing.Point(120, 0);
+            this.textBoxCell2.Name = "textBoxCell2";
+            this.textBoxCell2.TabIndex = 1;
+            // 
+            // textBoxCell3
+            // 
+            this.textBoxCell3.Location = new System.Drawing.Point(200, 0);
+            this.textBoxCell3.Name = "textBoxCell3";
+            this.textBoxCell3.TabIndex = 2;
+            // 
+            // textBoxCell4
+            // 
+            this.textBoxCell4.Location = new System.Drawing.Point(280, 0);
+            this.textBoxCell4.Name = "textBoxCell4";
+            this.textBoxCell4.TabIndex = 3;
+            // 
+            // textBoxCell5
+            // 
+            this.textBoxCell5.Location = new System.Drawing.Point(360, 0);
+            this.textBoxCell5.Name = "textBoxCell5";
+            this.textBoxCell5.TabIndex = 4;
+            // 
+            // textBoxCell6
+            // 
+            this.textBoxCell6.Location = new System.Drawing.Point(440, 0);
+            this.textBoxCell6.Name = "textBoxCell6";
+            this.textBoxCell6.TabIndex = 5;
+            // 
+            // textBoxCell7
+            // 
+            this.textBoxCell7.Location = new System.Drawing.Point(520, 0);
+            this.textBoxCell7.Name = "textBoxCell7";
+            this.textBoxCell7.TabIndex = 6;
+            // 
+            // textBoxCell8
+            // 
+            this.textBoxCell8.Location = new System.Drawing.Point(600, 0);
+            this.textBoxCell8.Name = "textBoxCell8";
+            this.textBoxCell8.TabIndex = 7;
+            // 
+            // textBoxCell9
+            // 
+            this.textBoxCell9.Location = new System.Drawing.Point(680, 0);
+            this.textBoxCell9.Name = "textBoxCell9";
+            this.textBoxCell9.TabIndex = 8;
+            // 
+            // textBoxCell10
+            // 
+            this.textBoxCell10.Location = new System.Drawing.Point(760, 0);
+            this.textBoxCell10.Name = "textBoxCell10";
+            this.textBoxCell10.TabIndex = 9;
+            // 
+            // rowHeaderCell1
+            // 
+            this.rowHeaderCell1.Location = new System.Drawing.Point(0, 0);
+            this.rowHeaderCell1.Name = "rowHeaderCell1";
+            this.rowHeaderCell1.Size = new System.Drawing.Size(40, 21);
+            this.rowHeaderCell1.TabIndex = 10;
+            // 
+            // ユニット明細参照テンプレート
+            // 
+            this.ColumnHeaders.AddRange(new GrapeCity.Win.MultiRow.ColumnHeaderSection[] {
+                        this.columnHeaderSection1});
+            this.Height = 42;
+            // 
+            // Row
+            // 
+            this.Row.Cells.Add(this.textBoxCell1);
+            this.Row.Cells.Add(this.textBoxCell2);
+            this.Row.Cells.Add(this.textBoxCell3);
+            this.Row.Cells.Add(this.textBoxCell4);
+            this.Row.Cells.Add(this.textBoxCell5);
+            this.Row.Cells.Add(this.textBoxCell6);
+            this.Row.Cells.Add(this.textBoxCell7);
+            this.Row.Cells.Add(this.textBoxCell8);
+            this.Row.Cells.Add(this.textBoxCell9);
+            this.Row.Cells.Add(this.textBoxCell10);
+            this.Row.Cells.Add(this.rowHeaderCell1);
+            this.Row.Height = 21;
+            this.Row.Width = 840;
+            this.Width = 840;
+        }
+        
+
+        #endregion
+
+        private GrapeCity.Win.MultiRow.ColumnHeaderSection columnHeaderSection1;
+        private GrapeCity.Win.MultiRow.ColumnHeaderCell columnHeaderCell1;
+        private GrapeCity.Win.MultiRow.ColumnHeaderCell columnHeaderCell2;
+        private GrapeCity.Win.MultiRow.ColumnHeaderCell columnHeaderCell3;
+        private GrapeCity.Win.MultiRow.ColumnHeaderCell columnHeaderCell4;
+        private GrapeCity.Win.MultiRow.ColumnHeaderCell columnHeaderCell5;
+        private GrapeCity.Win.MultiRow.ColumnHeaderCell columnHeaderCell6;
+        private GrapeCity.Win.MultiRow.ColumnHeaderCell columnHeaderCell7;
+        private GrapeCity.Win.MultiRow.ColumnHeaderCell columnHeaderCell8;
+        private GrapeCity.Win.MultiRow.ColumnHeaderCell columnHeaderCell9;
+        private GrapeCity.Win.MultiRow.ColumnHeaderCell columnHeaderCell10;
+        private GrapeCity.Win.MultiRow.CornerHeaderCell cornerHeaderCell1;
+        private GrapeCity.Win.MultiRow.TextBoxCell textBoxCell1;
+        private GrapeCity.Win.MultiRow.TextBoxCell textBoxCell2;
+        private GrapeCity.Win.MultiRow.TextBoxCell textBoxCell3;
+        private GrapeCity.Win.MultiRow.TextBoxCell textBoxCell4;
+        private GrapeCity.Win.MultiRow.TextBoxCell textBoxCell5;
+        private GrapeCity.Win.MultiRow.TextBoxCell textBoxCell6;
+        private GrapeCity.Win.MultiRow.TextBoxCell textBoxCell7;
+        private GrapeCity.Win.MultiRow.TextBoxCell textBoxCell8;
+        private GrapeCity.Win.MultiRow.TextBoxCell textBoxCell9;
+        private GrapeCity.Win.MultiRow.TextBoxCell textBoxCell10;
+        private GrapeCity.Win.MultiRow.RowHeaderCell rowHeaderCell1;
+        
+    }
+}
---
+++ MultiRowDesigner/ユニット明細参照テンプレート.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Text;
+using System.Windows.Forms;
+using GrapeCity.Win.MultiRow;
+
+namespace MultiRowDesigner
+{
+    public sealed partial class ユニット明細参照テンプレート : Template
+    {
+        public ユニット明細参照テンプレート()
+        {
+            InitializeComponent();
+        }
+    }
+}
