5ab48976f2541ac5e26b415ea52504ee2fb91543
Kinoshita Daichi <kinoshita.d@hbm-web.co.jp>
2024-01-17 15:27:57 +0900

    F_顧客コード選択 作成

---
+++ Form/F_顧客コード選択.Designer.cs
@@ -0,0 +1,306 @@
+﻿namespace u_net
+{
+    partial class F_顧客コード選択
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            リスト = new DataGridView();
+            表示件数 = new TextBox();
+            フィルタ_ア = new Button();
+            フィルタ_カ = new Button();
+            フィルタ_全て = new Button();
+            フィルタ_abc = new Button();
+            フィルタ_ワ = new Button();
+            フィルタ_ラ = new Button();
+            フィルタ_ヤ = new Button();
+            フィルタ_マ = new Button();
+            フィルタ_ハ = new Button();
+            フィルタ_ナ = new Button();
+            フィルタ_タ = new Button();
+            フィルタ_サ = new Button();
+            キャンセルボタン = new Button();
+            label1 = new Label();
+            検索ボタン = new Button();
+            statusStrip1 = new StatusStrip();
+            toolStripStatusLabel1 = new ToolStripStatusLabel();
+            toolStripStatusLabel2 = new ToolStripStatusLabel();
+            ((System.ComponentModel.ISupportInitialize)リスト).BeginInit();
+            statusStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // リスト
+            // 
+            リスト.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            リスト.Location = new Point(10, 80);
+            リスト.Name = "リスト";
+            リスト.RowTemplate.Height = 25;
+            リスト.Size = new Size(650, 500);
+            リスト.TabIndex = 0;
+            リスト.CellMouseDoubleClick += リスト_CellMouseDoubleClick;
+            リスト.KeyDown += リスト_KeyDown;
+            // 
+            // 表示件数
+            // 
+            表示件数.Font = new Font("BIZ UDPゴシック", 9.75F, FontStyle.Regular, GraphicsUnit.Point);
+            表示件数.Location = new Point(83, 600);
+            表示件数.Name = "表示件数";
+            表示件数.ReadOnly = true;
+            表示件数.Size = new Size(100, 20);
+            表示件数.TabIndex = 0;
+            表示件数.TabStop = false;
+            // 
+            // フィルタ_ア
+            // 
+            フィルタ_ア.Font = new Font("BIZ UDPゴシック", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            フィルタ_ア.Location = new Point(10, 40);
+            フィルタ_ア.Name = "フィルタ_ア";
+            フィルタ_ア.Size = new Size(50, 30);
+            フィルタ_ア.TabIndex = 0;
+            フィルタ_ア.Text = "ア";
+            フィルタ_ア.Click += フィルタ_ア_Click;
+            // 
+            // フィルタ_カ
+            // 
+            フィルタ_カ.Font = new Font("BIZ UDPゴシック", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            フィルタ_カ.Location = new Point(60, 40);
+            フィルタ_カ.Name = "フィルタ_カ";
+            フィルタ_カ.Size = new Size(50, 30);
+            フィルタ_カ.TabIndex = 0;
+            フィルタ_カ.Text = "カ";
+            フィルタ_カ.Click += フィルタ_カ_Click;
+            // 
+            // フィルタ_全て
+            // 
+            フィルタ_全て.Font = new Font("BIZ UDPゴシック", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            フィルタ_全て.Location = new Point(560, 40);
+            フィルタ_全て.Name = "フィルタ_全て";
+            フィルタ_全て.Size = new Size(50, 30);
+            フィルタ_全て.TabIndex = 0;
+            フィルタ_全て.Text = "全て";
+            フィルタ_全て.Click += フィルタ_全て_Click;
+            // 
+            // フィルタ_abc
+            // 
+            フィルタ_abc.Font = new Font("BIZ UDPゴシック", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            フィルタ_abc.Location = new Point(510, 40);
+            フィルタ_abc.Name = "フィルタ_abc";
+            フィルタ_abc.Size = new Size(50, 30);
+            フィルタ_abc.TabIndex = 0;
+            フィルタ_abc.Text = "abc";
+            フィルタ_abc.Click += フィルタ_abc_Click;
+            // 
+            // フィルタ_ワ
+            // 
+            フィルタ_ワ.Font = new Font("BIZ UDPゴシック", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            フィルタ_ワ.Location = new Point(460, 40);
+            フィルタ_ワ.Name = "フィルタ_ワ";
+            フィルタ_ワ.Size = new Size(50, 30);
+            フィルタ_ワ.TabIndex = 0;
+            フィルタ_ワ.Text = "ワ";
+            フィルタ_ワ.Click += フィルタ_ワ_Click;
+            // 
+            // フィルタ_ラ
+            // 
+            フィルタ_ラ.Font = new Font("BIZ UDPゴシック", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            フィルタ_ラ.Location = new Point(410, 40);
+            フィルタ_ラ.Name = "フィルタ_ラ";
+            フィルタ_ラ.Size = new Size(50, 30);
+            フィルタ_ラ.TabIndex = 0;
+            フィルタ_ラ.Text = "ラ";
+            フィルタ_ラ.Click += フィルタ_ラ_Click;
+            // 
+            // フィルタ_ヤ
+            // 
+            フィルタ_ヤ.Font = new Font("BIZ UDPゴシック", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            フィルタ_ヤ.Location = new Point(360, 40);
+            フィルタ_ヤ.Name = "フィルタ_ヤ";
+            フィルタ_ヤ.Size = new Size(50, 30);
+            フィルタ_ヤ.TabIndex = 0;
+            フィルタ_ヤ.Text = "ヤ";
+            フィルタ_ヤ.Click += フィルタ_ヤ_Click;
+            // 
+            // フィルタ_マ
+            // 
+            フィルタ_マ.Font = new Font("BIZ UDPゴシック", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            フィルタ_マ.Location = new Point(310, 40);
+            フィルタ_マ.Name = "フィルタ_マ";
+            フィルタ_マ.Size = new Size(50, 30);
+            フィルタ_マ.TabIndex = 0;
+            フィルタ_マ.Text = "マ";
+            フィルタ_マ.Click += フィルタ_マ_Click;
+            // 
+            // フィルタ_ハ
+            // 
+            フィルタ_ハ.Font = new Font("BIZ UDPゴシック", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            フィルタ_ハ.Location = new Point(260, 40);
+            フィルタ_ハ.Name = "フィルタ_ハ";
+            フィルタ_ハ.Size = new Size(50, 30);
+            フィルタ_ハ.TabIndex = 0;
+            フィルタ_ハ.Text = "ハ";
+            フィルタ_ハ.Click += フィルタ_ハ_Click;
+            // 
+            // フィルタ_ナ
+            // 
+            フィルタ_ナ.Font = new Font("BIZ UDPゴシック", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            フィルタ_ナ.Location = new Point(210, 40);
+            フィルタ_ナ.Name = "フィルタ_ナ";
+            フィルタ_ナ.Size = new Size(50, 30);
+            フィルタ_ナ.TabIndex = 0;
+            フィルタ_ナ.Text = "ナ";
+            フィルタ_ナ.Click += フィルタ_ナ_Click;
+            // 
+            // フィルタ_タ
+            // 
+            フィルタ_タ.Font = new Font("BIZ UDPゴシック", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            フィルタ_タ.Location = new Point(160, 40);
+            フィルタ_タ.Name = "フィルタ_タ";
+            フィルタ_タ.Size = new Size(50, 30);
+            フィルタ_タ.TabIndex = 0;
+            フィルタ_タ.Text = "タ";
+            フィルタ_タ.Click += フィルタ_タ_Click;
+            // 
+            // フィルタ_サ
+            // 
+            フィルタ_サ.Font = new Font("BIZ UDPゴシック", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            フィルタ_サ.Location = new Point(110, 40);
+            フィルタ_サ.Name = "フィルタ_サ";
+            フィルタ_サ.Size = new Size(50, 30);
+            フィルタ_サ.TabIndex = 0;
+            フィルタ_サ.Text = "サ";
+            フィルタ_サ.Click += フィルタ_サ_Click;
+            // 
+            // キャンセルボタン
+            // 
+            キャンセルボタン.Font = new Font("BIZ UDPゴシック", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            キャンセルボタン.Location = new Point(490, 10);
+            キャンセルボタン.Name = "キャンセルボタン";
+            キャンセルボタン.Size = new Size(119, 24);
+            キャンセルボタン.TabIndex = 1;
+            キャンセルボタン.Text = "キャンセル(&C)";
+            キャンセルボタン.Click += キャンセルボタン_Click;
+            // 
+            // label1
+            // 
+            label1.Font = new Font("BIZ UDPゴシック", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(18, 600);
+            label1.Name = "label1";
+            label1.Size = new Size(63, 20);
+            label1.TabIndex = 2;
+            label1.Text = "表示件数";
+            label1.TextAlign = ContentAlignment.MiddleLeft;
+            // 
+            // 検索ボタン
+            // 
+            検索ボタン.Enabled = false;
+            検索ボタン.Font = new Font("BIZ UDPゴシック", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            検索ボタン.Location = new Point(370, 10);
+            検索ボタン.Name = "検索ボタン";
+            検索ボタン.Size = new Size(119, 24);
+            検索ボタン.TabIndex = 3;
+            検索ボタン.Text = "詳細検索(&S)";
+            検索ボタン.Click += 検索ボタン_Click;
+            // 
+            // statusStrip1
+            // 
+            statusStrip1.Items.AddRange(new ToolStripItem[] { toolStripStatusLabel1, toolStripStatusLabel2 });
+            statusStrip1.Location = new Point(0, 633);
+            statusStrip1.Name = "statusStrip1";
+            statusStrip1.Padding = new Padding(1, 0, 19, 0);
+            statusStrip1.Size = new Size(672, 22);
+            statusStrip1.TabIndex = 111;
+            statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            toolStripStatusLabel1.Size = new Size(0, 17);
+            // 
+            // toolStripStatusLabel2
+            // 
+            toolStripStatusLabel2.Font = new Font("BIZ UDPゴシック", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            toolStripStatusLabel2.Name = "toolStripStatusLabel2";
+            toolStripStatusLabel2.Size = new Size(89, 17);
+            toolStripStatusLabel2.Text = "各種項目の説明";
+            // 
+            // F_顧客コード選択
+            // 
+            ClientSize = new Size(672, 655);
+            Controls.Add(statusStrip1);
+            Controls.Add(検索ボタン);
+            Controls.Add(label1);
+            Controls.Add(キャンセルボタン);
+            Controls.Add(表示件数);
+            Controls.Add(リスト);
+            Controls.Add(フィルタ_ア);
+            Controls.Add(フィルタ_カ);
+            Controls.Add(フィルタ_全て);
+            Controls.Add(フィルタ_abc);
+            Controls.Add(フィルタ_ワ);
+            Controls.Add(フィルタ_ラ);
+            Controls.Add(フィルタ_ヤ);
+            Controls.Add(フィルタ_マ);
+            Controls.Add(フィルタ_ハ);
+            Controls.Add(フィルタ_ナ);
+            Controls.Add(フィルタ_タ);
+            Controls.Add(フィルタ_サ);
+            Name = "F_顧客コード選択";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "コード選択";
+            FormClosing += F_検索_FormClosing;
+            Load += Form_Load;
+            ((System.ComponentModel.ISupportInitialize)リスト).EndInit();
+            statusStrip1.ResumeLayout(false);
+            statusStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+
+        #endregion
+
+        private DataGridView リスト;
+        private TextBox 表示件数;
+        private Button フィルタ_ア;
+        private Button フィルタ_カ;
+        private Button フィルタ_全て;
+        private Button フィルタ_abc;
+        private Button フィルタ_ワ;
+        private Button フィルタ_ラ;
+        private Button フィルタ_ヤ;
+        private Button フィルタ_マ;
+        private Button フィルタ_ハ;
+        private Button フィルタ_ナ;
+        private Button フィルタ_タ;
+        private Button フィルタ_サ;
+        private Button キャンセルボタン;
+        private Label label1;
+        private Button 検索ボタン;
+        private StatusStrip statusStrip1;
+        private ToolStripStatusLabel toolStripStatusLabel1;
+        private ToolStripStatusLabel toolStripStatusLabel2;
+    }
+}
---
+++ Form/F_顧客コード選択.cs
@@ -0,0 +1,520 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using Microsoft.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using u_net.Public;
+using Microsoft.Data.SqlClient;
+
+namespace u_net
+{
+    public partial class F_顧客コード選択 : Form
+    {
+
+
+        private Control previousControl;
+        private SqlConnection cn;
+        private SqlTransaction tx;
+        private string BASE_CAPTION = "コード選択";
+
+
+        private object objArgs;
+        private int intWindowHeight;   // 現在保持している高さ
+        private int intWindowWidth;    // 現在保持している幅
+        private TextBox objArgs1;      // 保存用オブジェクト１
+        //private MSHierarchicalFlexGridLib.MSHFlexGrid objArgs2; // 保存用オブジェクト２
+
+
+
+        public string FilterName;
+        private int FilterNumber = 1;
+        public string SelectedCode;
+
+
+
+
+        public F_顧客コード選択()
+        {
+            InitializeComponent();
+        }
+
+
+        public void Connect()
+        {
+            Connection connectionInfo = new Connection();
+            string connectionString = connectionInfo.Getconnect();
+            cn = new SqlConnection(connectionString);
+            cn.Open();
+        }
+
+        private void Filtering(object filterNumber, string FilterName)
+        {
+            // リストを指定条件で更新する
+            // filterNumber - 抽出番号
+            // FilterName   - 抽出対象となるフィールド名
+            Connect();
+            string strFilter = "";
+            string query = "";
+
+            switch (filterNumber)
+            {
+                case 1:
+                    strFilter = "WHERE " + FilterName + " like '[アイウエオ]%'";
+                    break;
+                case 2:
+                    strFilter = "WHERE " + FilterName + " like '[カキクケコガギグゲゴ]%'";
+                    break;
+                case 3:
+                    strFilter = "WHERE " + FilterName + " like '[サシスセソザジズゼゾ]%'";
+                    break;
+                case 4:
+                    strFilter = "WHERE " + FilterName + " like '[タチツテトダヂヅデド]%'";
+                    break;
+                case 5:
+                    strFilter = "WHERE " + FilterName + " like '[ナニヌネノ]%'";
+                    break;
+                case 6:
+                    strFilter = "WHERE " + FilterName + " like '[ハヒフヘホバビブベボパピプペポ]%'";
+                    break;
+                case 7:
+                    strFilter = "WHERE " + FilterName + " like '[マミムメモ]%'";
+                    break;
+                case 8:
+                    strFilter = "WHERE " + FilterName + " like '[ヤユヨ]%'";
+                    break;
+                case 9:
+                    strFilter = "WHERE " + FilterName + " like '[ラリルレロ]%'";
+                    break;
+                case 10:
+                    strFilter = "WHERE " + FilterName + " like '[ワヲン]%'";
+                    break;
+                case 11:
+                    strFilter = "WHERE " + FilterName + " like '[abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ]%'";
+                    break;
+                case 12:
+                    strFilter = "";
+                    break;
+                default:
+                    MessageBox.Show("不正な呼び出しが行われました.", "Filtering", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    break;
+            }
+
+            // 一覧のソースを設定する
+            // 各ソースに対応するビューが必要
+            switch (FilterName)
+            {
+                case "申請顧客名フリガナ":
+                    query = "SELECT 顧客コード, 顧客名, 顧客担当者名 FROM V顧客検索_申請 " + strFilter + " ORDER BY " + FilterName + ";";
+                    break;
+                case "顧客名フリガナ":
+                    query = "SELECT 顧客コード, 顧客名, 顧客担当者名, 無効 FROM V顧客検索 " + strFilter + " ORDER BY " + FilterName + ";";
+                    break;
+                case "仕入先名フリガナ":
+                    query = "SELECT 仕入先コード, 仕入先名, 電話番号 FROM V仕入先検索 " + strFilter + " ORDER BY " + FilterName + ";";
+                    break;
+                case "メーカー名フリガナ":
+                    query = "SELECT メーカーコード, メーカー名, 電話番号 FROM uv_メーカー検索 " + strFilter + " ORDER BY " + FilterName + ";";
+                    break;
+                case "支払先名フリガナ":
+                    query = "SELECT 支払先コード, 支払先名, 電話番号 FROM V支払先検索 " + strFilter + " ORDER BY " + FilterName + ";";
+                    break;
+            }
+
+            
+            SqlDataAdapter adapter = new SqlDataAdapter(query, cn);
+            DataTable dataTable = new DataTable();
+            adapter.Fill(dataTable);
+
+            // DataGridViewにデータをバインド
+            リスト.DataSource = dataTable;
+
+
+            // 件数を表示する
+            表示件数.Text = リスト.RowCount.ToString();
+
+            //選択ボタンをEnable=falseにする
+            Enable_Switch();
+
+            リスト.Focus(); // トグルボタンがクリックされた場合の処理
+
+            if (リスト.RowCount > 0)
+            {
+                リスト.Rows[0].Selected = true;
+            }
+
+
+            // 値が確定済みであれば何もしない
+            //switch (objArgs.GetType().Name)
+            //{
+            //    case "TextBox":
+            //        if (objArgs1.Text != null)
+            //        {
+            //            return;
+            //        }
+            //        break;
+            //    case "MSHFlexGrid":
+            //        if (objArgs2.Text != null)
+            //        {
+            //            return;
+            //        }
+            //        break;
+            //}
+
+            // 値が未確定であれば各抽出リストの一番目の項目を選択する
+            //if (リスト.RowCount > 0)
+            //{
+            //    リスト.Selected[1] = true;
+            //    リスト.Value = リスト.Column[0, 1];
+            //}
+
+            
+        }
+
+
+
+        public void Form_Load(object sender, EventArgs e)
+        {
+
+            MyApi myapi = new MyApi();
+
+            if (string.IsNullOrEmpty(FilterName))
+            {
+                MessageBox.Show("呼び出しに失敗しました。\n管理者に連絡してください。", this.Name, MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                DialogResult = DialogResult.Cancel;
+                this.Close();
+                return;
+            }
+
+            // ウィンドウサイズを調整する
+            //int lngX, lngy;
+            //myapi.GetFullScreen(out lngX, out lngy);
+            //intWindowHeight = this.Height;
+            //intWindowWidth = this.Width;
+            //this.Width = intWindowWidth;
+            //this.Height = lngy * myapi.GetTwipPerDot(myapi.GetLogPixel()) - 1200;
+            //Form_Resize(sender, e);
+
+            //実行中フォーム起動
+            //string LoginUserCode = CommonConstants.LoginUserCode;
+            //LocalSetting localSetting = new LocalSetting();
+            //localSetting.LoadPlace(LoginUserCode, this);
+
+
+            // 一覧を表示する
+            Filtering(FilterNumber, FilterName);
+
+            //列幅を自動調整する
+            リスト.AutoResizeColumns(DataGridViewAutoSizeColumnsMode.AllCells);
+
+            //選択範囲を行全体に設定
+            リスト.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+
+            if (リスト.RowCount > 0)
+            {
+                リスト.Rows[0].Selected = true;
+            }
+
+            リスト.ReadOnly = true;
+            リスト.AllowUserToAddRows = false;
+            リスト.AllowUserToDeleteRows = false;
+
+            toolStripStatusLabel2.Text = "■確定するには、確定したい項目をダブルクリックするか、選択後[Enter]キーを押下します。　■[Function]キーあるいは←→キーで抽出条件を変更します。";
+
+        }
+
+
+        private void F_検索_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            //string LoginUserCode = CommonConstants.LoginUserCode;
+            //LocalSetting test = new LocalSetting();
+            //test.SavePlace(LoginUserCode, this);
+        }
+
+
+        //private void Form_Resize(object sender, EventArgs e)
+        //{
+        //    this.SuspendLayout();
+        //    リスト.Height += (this.Height - intWindowHeight);
+        //    リスト.Width += (this.Width - intWindowWidth);
+        //    intWindowHeight = this.Height;
+        //    intWindowWidth = this.Width;
+        //    this.ResumeLayout();
+        //}
+
+        private void リスト_CellMouseDoubleClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            try
+            {
+                //switch (objArgs.GetType().Name)
+                //{
+                //    case "TextBox":
+                //        objArgs1.Text = リスト.Value;
+                //        break;
+                //    case "MSHFlexGrid":
+                //        objArgs2.text = リスト.Value;
+                //        break;
+                //}
+
+
+                if (e.RowIndex >= 0)
+                {
+                    DataGridViewRow selectedRow = リスト.Rows[e.RowIndex];
+
+                    // 選択した行の各セルの値を取得
+                    string column1Value = selectedRow.Cells[0].Value.ToString(); // 0は列のインデックス
+
+                    SelectedCode = column1Value;
+
+                    DialogResult = DialogResult.OK;
+                    Close();
+                }
+
+
+
+            }
+            catch (Exception ex)
+            {
+                if (ex.HResult == 2135)
+                {
+                    MessageBox.Show("入力できない状態にあるため、設定できません。", this.Text, MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                }
+                else
+                {
+                    Console.WriteLine(this.Name + "_リスト_CellMouseDoubleClick - " + ex.HResult + " : " + ex.Message);
+                }
+            }
+        }
+
+        private void リスト_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Enter:
+                    if (リスト.SelectedRows.Count > 0)
+                    {
+                        // 選択されている行を取得
+                        DataGridViewRow selectedRow = リスト.SelectedRows[0];
+
+                        // 選択した行の各セルの値を取得
+                        string column1Value = selectedRow.Cells[0].Value.ToString(); // 0は列のインデックス
+
+                        SelectedCode = column1Value;
+
+                        DialogResult = DialogResult.OK;
+                        Close();
+                    }
+                    break;
+                case Keys.Right:
+                    FilterNumber = (FilterNumber % 12) + 1;
+                    Filtering(FilterNumber, FilterName);
+                    break;
+                case Keys.Left:
+                    FilterNumber = (FilterNumber + (12 - 2)) % 12 + 1;
+                    Filtering(FilterNumber, FilterName);
+                    break;
+                case Keys.F1:
+                    フィルタ_ア_Click(sender,e);
+                    break;
+                case Keys.F2:
+                    フィルタ_カ_Click(sender, e);
+                    break;
+                case Keys.F3:
+                    フィルタ_サ_Click(sender, e);
+                    break;
+                case Keys.F4:
+                    フィルタ_タ_Click(sender, e);
+                    break;
+                case Keys.F5:
+                    フィルタ_ナ_Click(sender, e);
+                    break;
+                case Keys.F6:
+                    フィルタ_ハ_Click(sender, e);
+                    break;
+                case Keys.F7:
+                    フィルタ_マ_Click(sender, e);
+                    break;
+                case Keys.F8:
+                    フィルタ_ヤ_Click(sender, e);
+                    break;
+                case Keys.F9:
+                    フィルタ_ラ_Click(sender, e);
+                    break;
+                case Keys.F10:
+                    フィルタ_ワ_Click(sender, e);
+                    break;
+                case Keys.F11:
+                    フィルタ_abc_Click(sender, e);
+                    break;
+                case Keys.F12:
+                    フィルタ_全て_Click(sender, e);
+                    break;
+                
+            }
+        }
+
+
+        private void キャンセルボタン_Click(object sender, EventArgs e)
+        {
+
+            DialogResult = DialogResult.Cancel;
+            this.Close();
+        }
+
+        private void 検索ボタン_Click(object sender, EventArgs e)
+        {
+            リスト.Focus();
+            // "顧客検索設定" フォームを開く
+            //Form searchForm = new Form();
+            //searchForm.Show();
+        }
+
+        private void Enable_Switch()
+        {
+
+            フィルタ_ア.Enabled = true;
+            フィルタ_カ.Enabled = true;
+            フィルタ_サ.Enabled = true;
+            フィルタ_タ.Enabled = true;
+            フィルタ_ナ.Enabled = true;
+            フィルタ_ハ.Enabled = true;
+            フィルタ_マ.Enabled = true;
+            フィルタ_ヤ.Enabled = true;
+            フィルタ_ラ.Enabled = true;
+            フィルタ_ワ.Enabled = true;
+            フィルタ_abc.Enabled = true;
+            フィルタ_全て.Enabled = true;
+
+            switch (FilterNumber)
+            {
+                case 1:
+                    フィルタ_ア.Enabled = false;
+                    break;
+                case 2:
+                    フィルタ_カ.Enabled = false;
+                    break;
+                case 3:
+                    フィルタ_サ.Enabled = false;
+                    break;
+                case 4:
+                    フィルタ_タ.Enabled = false;
+                    break;
+                case 5:
+                    フィルタ_ナ.Enabled = false;
+                    break;
+                case 6:
+                    フィルタ_ハ.Enabled = false;
+                    break;
+                case 7:
+                    フィルタ_マ.Enabled = false;
+                    break;
+                case 8:
+                    フィルタ_ヤ.Enabled = false;
+                    break;
+                case 9:
+                    フィルタ_ラ.Enabled = false;
+                    break;
+                case 10:
+                    フィルタ_ワ.Enabled = false;
+                    break;
+                case 11:
+                    フィルタ_abc.Enabled = false;
+                    break;
+                case 12:
+                    フィルタ_全て.Enabled = false;
+                    break;
+            }
+
+
+
+        }
+
+       
+
+        private void フィルタ_ア_Click(object sender, EventArgs e)
+        {
+            FilterNumber = 1;
+            Filtering(FilterNumber, FilterName);
+        }
+
+        private void フィルタ_カ_Click(object sender, EventArgs e)
+        {
+            FilterNumber = 2;
+            Filtering(FilterNumber, FilterName);
+        }
+
+        private void フィルタ_サ_Click(object sender, EventArgs e)
+        {
+            FilterNumber = 3;
+            Filtering(FilterNumber, FilterName);
+        }
+
+        private void フィルタ_タ_Click(object sender, EventArgs e)
+        {
+            FilterNumber = 4;
+            Filtering(FilterNumber, FilterName);
+        }
+
+        private void フィルタ_ナ_Click(object sender, EventArgs e)
+        {
+            FilterNumber = 5;
+            Filtering(FilterNumber, FilterName);
+        }
+
+        private void フィルタ_ハ_Click(object sender, EventArgs e)
+        {
+            FilterNumber = 6;
+            Filtering(FilterNumber, FilterName);
+        }
+
+        private void フィルタ_マ_Click(object sender, EventArgs e)
+        {
+            FilterNumber = 7;
+            Filtering(FilterNumber, FilterName);
+        }
+
+        private void フィルタ_ヤ_Click(object sender, EventArgs e)
+        {
+            FilterNumber = 8;
+            Filtering(FilterNumber, FilterName);
+        }
+
+        private void フィルタ_ラ_Click(object sender, EventArgs e)
+        {
+            FilterNumber = 9;
+            Filtering(FilterNumber, FilterName);
+        }
+
+        private void フィルタ_ワ_Click(object sender, EventArgs e)
+        {
+            FilterNumber = 10;
+            Filtering(FilterNumber, FilterName);
+        }
+
+        private void フィルタ_abc_Click(object sender, EventArgs e)
+        {
+            FilterNumber = 11;
+            Filtering(FilterNumber, FilterName);
+        }
+
+        private void フィルタ_全て_Click(object sender, EventArgs e)
+        {
+            FilterNumber = 12;
+            Filtering(FilterNumber, FilterName);
+        }
+
+        
+    }
+
+
+
+
+
+
+
+}
+
