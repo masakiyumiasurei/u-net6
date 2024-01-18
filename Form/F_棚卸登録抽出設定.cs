13ea7e85c75116f24392d4edb257d023cc179dd6
Kinoshita Daichi <kinoshita.d@hbm-web.co.jp>
2024-01-18 17:23:08 +0900

    F_棚卸登録抽出設定 作成

---
+++ Form/F_棚卸登録抽出設定.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using u_net.Public;
+
+namespace u_net
+{
+    public partial class F_棚卸登録抽出設定 : Form
+    {
+
+        public F_棚卸登録抽出設定()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection cn;
+
+        public void Connect()
+        {
+            Connection connectionInfo = new Connection();
+            string connectionString = connectionInfo.Getconnect();
+            cn = new SqlConnection(connectionString);
+            cn.Open();
+        }
+
+        private void Form_Load(object sender, EventArgs e)
+        {
+            try
+            {
+
+                foreach (Control control in Controls)
+                {
+                    control.PreviewKeyDown += OriginalClass.ValidateCheck;
+                }
+
+
+                // 対象フォームが読み込まれていないときはすぐに終了する
+                if (Application.OpenForms["F_棚卸登録"] == null)
+                {
+                    MessageBox.Show("[棚卸登録]画面が起動していない状態では実行できません。", "警告", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    this.Close();
+                    return;
+                }
+
+                //開いているフォームのインスタンスを作成する
+                F_棚卸登録 frmTarget = Application.OpenForms.OfType<F_棚卸登録>().FirstOrDefault();
+
+                品名.Text = frmTarget.str品名;
+                型番.Text = frmTarget.str型番;
+
+            }
+            catch (Exception ex)
+            {
+                // 例外処理が必要な場合はここで処理を追加
+                MessageBox.Show("エラーが発生しました: " + ex.Message, "エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void 抽出ボタン_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                F_棚卸登録? frmTarget = Application.OpenForms.OfType<F_棚卸登録>().FirstOrDefault();
+
+
+                frmTarget.str品名 = Nz(品名.Text);
+                frmTarget.str型番 = Nz(型番.Text);
+
+                long cnt = frmTarget.DoUpdate();
+
+                if (cnt == 0)
+                {
+                    MessageBox.Show("抽出条件に一致するデータはありません。", this.Text, MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                    return;
+                }
+                else if (cnt < 0)
+                {
+                    MessageBox.Show("エラーが発生したため、抽出できませんでした。", this.Text, MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    return;
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(this.Name + "_抽出ボタン_Click - " + ex.Message);
+                MessageBox.Show("エラーが発生しました。", this.Text, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                //this.Painting = true;
+                this.Close();
+            }
+        }
+
+        private void キャンセルボタン_MouseClick(object sender, MouseEventArgs e)
+        {
+            this.Close();
+        }
+
+        // Nz メソッドの代替
+        private T Nz<T>(T value)
+        {
+            if (value == null)
+            {
+                return default(T);
+            }
+            return value;
+        }
+
+        private void F_棚卸登録抽出設定_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Return:
+                    SelectNextControl(ActiveControl, true, true, true, true);
+                    break;
+            }
+        }
+    }
+}
