3381aefd84e92353c9d7239e950de3b5307502a1
Kinoshita Daichi <kinoshita.d@hbm-web.co.jp>
2024-01-25 18:00:02 +0900

    F_文書 作成

--- Form/F_文書.cs
+++ Form/F_文書.cs
@@ -8130,6 +8130,322 @@ namespace u_net
 
         }
 
-#endregion
+        #endregion
+
+
+        #region 不具合調査修理依頼書
+
+        private void 不具合調査修理依頼書_受付日_DoubleClick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void 不具合調査修理依頼書_受付日_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+        }
+
+        private void 不具合調査修理依頼書_受付日_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void 不具合調査修理依頼書_受付日_Validating(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void 不具合調査修理依頼書_受付日選択ボタン_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void 不具合調査修理依頼書_受注コード_DoubleClick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void 不具合調査修理依頼書_受注コード_KeyDown(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void 不具合調査修理依頼書_受注コード_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+        }
+
+        private void 不具合調査修理依頼書_受注コード_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void 不具合調査修理依頼書_受注コード_Validating(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void 不具合調査修理依頼書_受注コード参照ボタン_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void 不具合調査修理依頼書_受注コード参照ボタン_Enter(object sender, EventArgs e)
+        {
+            toolStripStatusLabel1.Text = "■対象コードの情報を表示。版が登録されている情報は最新のものを表示。";
+        }
+
+        private void 不具合調査修理依頼書_受注コード参照ボタン_Leave(object sender, EventArgs e)
+        {
+            toolStripStatusLabel1.Text = "各種項目の説明";
+        }
+
+        private void 不具合調査修理依頼書_顧客コード_DoubleClick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void 不具合調査修理依頼書_顧客コード_KeyDown(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void 不具合調査修理依頼書_顧客コード_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+        }
+
+        private void 不具合調査修理依頼書_顧客コード_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void 不具合調査修理依頼書_顧客コード_Validating(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void 不具合調査修理依頼書_顧客コード選択ボタン_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void 不具合調査修理依頼書_顧客コード選択ボタン_Enter(object sender, EventArgs e)
+        {
+            toolStripStatusLabel1.Text = "■顧客検索ダイアログを表示。";
+        }
+
+        private void 不具合調査修理依頼書_顧客コード選択ボタン_Leave(object sender, EventArgs e)
+        {
+            toolStripStatusLabel1.Text = "各種項目の説明";
+        }
+
+        private void 不具合調査修理依頼書_顧客参照ボタン_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void 不具合調査修理依頼書_顧客参照ボタン_Enter(object sender, EventArgs e)
+        {
+            toolStripStatusLabel1.Text = "■顧客データを参照します。";
+        }
+
+        private void 不具合調査修理依頼書_顧客参照ボタン_Leave(object sender, EventArgs e)
+        {
+            toolStripStatusLabel1.Text = "各種項目の説明";
+        }
+
+        private void 不具合調査修理依頼書_依頼分類_Validated(object sender, EventArgs e)
+        {
+
+        }
+
+        private void 不具合調査修理依頼書_依頼分類_Validating(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void 不具合調査修理依頼書_依頼分類その他_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void 不具合調査修理依頼書_依頼分類その他_Leave(object sender, EventArgs e)
+        {
+
+        }
+
+        private void 不具合調査修理依頼書_依頼分類その他_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void 不具合調査修理依頼書_依頼分類その他_Validating(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void 不具合調査修理依頼書_発生日_DoubleClick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void 不具合調査修理依頼書_発生日_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+        }
+
+        private void 不具合調査修理依頼書_発生日_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void 不具合調査修理依頼書_発生日_Validating(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void 不具合調査修理依頼書_発生日選択ボタン_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void 不具合調査修理依頼書_連絡者社名_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void 不具合調査修理依頼書_連絡者社名_Validating(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void 不具合調査修理依頼書_連絡者氏名_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void 不具合調査修理依頼書_連絡者氏名_Validating(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void 不具合調査修理依頼書_型番_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void 不具合調査修理依頼書_型番_Validating(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void 不具合調査修理依頼書_シリアル番号1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void 不具合調査修理依頼書_シリアル番号1_Validating(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void 不具合調査修理依頼書_シリアル番号2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void 不具合調査修理依頼書_シリアル番号2_Validating(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void 不具合調査修理依頼書_数量_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void 不具合調査修理依頼書_数量_Validating(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void 不具合調査修理依頼書_発生場所_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void 不具合調査修理依頼書_発生場所_Validating(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void 不具合調査修理依頼書_現象_DoubleClick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void 不具合調査修理依頼書_現象_Enter(object sender, EventArgs e)
+        {
+            toolStripStatusLabel1.Text = "■全角２０００文字まで入力できます。　■入力欄をダブルクリックすると別ウィンドウに表示されます。";
+        }
+
+        private void 不具合調査修理依頼書_現象_Leave(object sender, EventArgs e)
+        {
+            toolStripStatusLabel1.Text = "各種項目の説明";
+        }
+
+        private void 不具合調査修理依頼書_現象_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void 不具合調査修理依頼書_現象_Validating(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void 不具合調査修理依頼書_顧客の声_DoubleClick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void 不具合調査修理依頼書_顧客の声_Enter(object sender, EventArgs e)
+        {
+            toolStripStatusLabel1.Text = "■全角２０００文字まで入力できます。　■入力欄をダブルクリックすると別ウィンドウに表示されます。";
+        }
+
+        private void 不具合調査修理依頼書_顧客の声_Leave(object sender, EventArgs e)
+        {
+            toolStripStatusLabel1.Text = "各種項目の説明";
+        }
+
+        private void 不具合調査修理依頼書_顧客の声_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void 不具合調査修理依頼書_顧客の声_Validating(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void 不具合調査修理依頼書_代替有無_Validated(object sender, EventArgs e)
+        {
+
+        }
+
+        private void 不具合調査修理依頼書_代替有無_Validating(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void 不具合調査修理依頼書_回答書ボタン_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        #endregion
+
     }
 }
--- Form/F_文書_作成用.cs
+++ Form/F_文書_作成用.cs
@@ -20,117 +20,302 @@ namespace u_net
             InitializeComponent();
         }
 
-        private void 品質異常報告書_発生日_DoubleClick(object sender, EventArgs e)
+        private void 不具合調査修理依頼書_受付日_DoubleClick(object sender, EventArgs e)
         {
 
         }
 
-        private void 品質異常報告書_発生日_KeyPress(object sender, KeyPressEventArgs e)
+        private void 不具合調査修理依頼書_受付日_KeyPress(object sender, KeyPressEventArgs e)
         {
 
         }
 
-        private void 品質異常報告書_発生日_TextChanged(object sender, EventArgs e)
+        private void 不具合調査修理依頼書_受付日_TextChanged(object sender, EventArgs e)
         {
 
         }
 
-        private void 品質異常報告書_発生日_Validated(object sender, EventArgs e)
+        private void 不具合調査修理依頼書_受付日_Validating(object sender, CancelEventArgs e)
         {
 
         }
 
-        private void 品質異常報告書_発生日_Validating(object sender, CancelEventArgs e)
+        private void 不具合調査修理依頼書_受注日選択ボタン_Click(object sender, EventArgs e)
         {
 
         }
 
-        private void 品質異常報告書_発生日選択ボタン_Click(object sender, EventArgs e)
+        private void 不具合調査修理依頼書_受注コード_DoubleClick(object sender, EventArgs e)
         {
 
         }
 
-        private void 品質異常報告書_発生場所_Enter(object sender, EventArgs e)
+        private void 不具合調査修理依頼書_受注コード_KeyDown(object sender, KeyEventArgs e)
         {
 
         }
 
-        private void 品質異常報告書_発生場所_Leave(object sender, EventArgs e)
+        private void 不具合調査修理依頼書_受注コード_KeyPress(object sender, KeyPressEventArgs e)
         {
 
         }
 
-        private void 品質異常報告書_発生場所_TextChanged(object sender, EventArgs e)
+        private void 不具合調査修理依頼書_受注コード_TextChanged(object sender, EventArgs e)
         {
 
         }
 
-        private void 品質異常報告書_発生場所_Validated(object sender, EventArgs e)
+        private void 不具合調査修理依頼書_受注コード_Validating(object sender, CancelEventArgs e)
         {
 
         }
 
-        private void 品質異常報告書_発生場所_Validating(object sender, CancelEventArgs e)
+        private void 不具合調査修理依頼書_受注コード参照ボタン_Click(object sender, EventArgs e)
         {
 
         }
 
-        private void 品質異常報告書_型番_Enter(object sender, EventArgs e)
+        private void 不具合調査修理依頼書_顧客コード_DoubleClick(object sender, EventArgs e)
         {
 
         }
 
-        private void 品質異常報告書_型番_Leave(object sender, EventArgs e)
+        private void 不具合調査修理依頼書_顧客コード_KeyDown(object sender, KeyEventArgs e)
         {
 
         }
 
-        private void 品質異常報告書_型番_TextChanged(object sender, EventArgs e)
+        private void 不具合調査修理依頼書_顧客コード_KeyPress(object sender, KeyPressEventArgs e)
         {
 
         }
 
-        private void 品質異常報告書_型番_Validated(object sender, EventArgs e)
+        private void 不具合調査修理依頼書_顧客コード_TextChanged(object sender, EventArgs e)
         {
 
         }
 
-        private void 品質異常報告書_型番_Validating(object sender, CancelEventArgs e)
+        private void 不具合調査修理依頼書_顧客コード_Validating(object sender, CancelEventArgs e)
         {
 
         }
 
-        private void 品質異常報告書_異常内容_DoubleClick(object sender, EventArgs e)
+        private void 不具合調査修理依頼書_顧客コード選択ボタン_Click(object sender, EventArgs e)
         {
 
         }
 
-        private void 品質異常報告書_異常内容_Enter(object sender, EventArgs e)
+        private void 不具合調査修理依頼書_顧客コード選択ボタン_Enter(object sender, EventArgs e)
         {
 
         }
 
-        private void 品質異常報告書_異常内容_Leave(object sender, EventArgs e)
+        private void 不具合調査修理依頼書_顧客コード選択ボタン_Leave(object sender, EventArgs e)
         {
 
         }
 
-        private void 品質異常報告書_異常内容_TextChanged(object sender, EventArgs e)
+        private void 不具合調査修理依頼書_顧客参照ボタン_Click(object sender, EventArgs e)
         {
 
         }
 
-        private void 品質異常報告書_異常内容_Validated(object sender, EventArgs e)
+        private void 不具合調査修理依頼書_顧客参照ボタン_Enter(object sender, EventArgs e)
         {
 
         }
 
-        private void 品質異常報告書_異常内容_Validating(object sender, CancelEventArgs e)
+        private void 不具合調査修理依頼書_顧客参照ボタン_Leave(object sender, EventArgs e)
         {
 
         }
 
-        private void 品質異常報告書_回答書ボタン_Click(object sender, EventArgs e)
+        private void 不具合調査修理依頼書_依頼分類_Validated(object sender, EventArgs e)
+        {
+
+        }
+
+        private void 不具合調査修理依頼書_依頼分類_Validating(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void 不具合調査修理依頼書_依頼分類その他_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void 不具合調査修理依頼書_依頼分類その他_Leave(object sender, EventArgs e)
+        {
+
+        }
+
+        private void 不具合調査修理依頼書_依頼分類その他_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void 不具合調査修理依頼書_依頼分類その他_Validating(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void 不具合調査修理依頼書_発生日_DoubleClick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void 不具合調査修理依頼書_発生日_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+        }
+
+        private void 不具合調査修理依頼書_発生日_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void 不具合調査修理依頼書_発生日_Validating(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void 不具合調査修理依頼書_発生日選択ボタン_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void 不具合調査修理依頼書_連絡者社名_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void 不具合調査修理依頼書_連絡者社名_Validating(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void 不具合調査修理依頼書_連絡者氏名_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void 不具合調査修理依頼書_連絡者氏名_Validating(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void 不具合調査修理依頼書_型番_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void 不具合調査修理依頼書_型番_Validating(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void 不具合調査修理依頼書_シリアル番号1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void 不具合調査修理依頼書_シリアル番号1_Validating(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void 不具合調査修理依頼書_シリアル番号2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void 不具合調査修理依頼書_シリアル番号2_Validating(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void 不具合調査修理依頼書_数量_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void 不具合調査修理依頼書_数量_Validating(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void 不具合調査修理依頼書_発生場所_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void 不具合調査修理依頼書_発生場所_Validating(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void 不具合調査修理依頼書_現象_DoubleClick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void 不具合調査修理依頼書_現象_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void 不具合調査修理依頼書_現象_Leave(object sender, EventArgs e)
+        {
+
+        }
+
+        private void 不具合調査修理依頼書_現象_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void 不具合調査修理依頼書_現象_Validating(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void 不具合調査修理依頼書_顧客の声_DoubleClick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void 不具合調査修理依頼書_顧客の声_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void 不具合調査修理依頼書_顧客の声_Leave(object sender, EventArgs e)
+        {
+
+        }
+
+        private void 不具合調査修理依頼書_顧客の声_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void 不具合調査修理依頼書_顧客の声_Validating(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void 不具合調査修理依頼書_代替有無_Validated(object sender, EventArgs e)
+        {
+
+        }
+
+        private void 不具合調査修理依頼書_代替有無_Validating(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void 不具合調査修理依頼書_回答書ボタン_Click(object sender, EventArgs e)
         {
 
         }
