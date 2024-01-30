e58befb49bf35b2b3ddbe9b0422e6ef1f5588e09
Kinoshita Daichi <kinoshita.d@hbm-web.co.jp>
2024-01-30 16:26:23 +0900

    F_検索デザイン修正

--- Form/F_検索.Designer.cs
+++ Form/F_検索.Designer.cs
@@ -58,7 +58,7 @@
             リスト.Location = new Point(10, 80);
             リスト.Name = "リスト";
             リスト.RowTemplate.Height = 25;
-            リスト.Size = new Size(650, 500);
+            リスト.Size = new Size(765, 500);
             リスト.TabIndex = 0;
             リスト.CellMouseDoubleClick += リスト_CellMouseDoubleClick;
             リスト.KeyDown += リスト_KeyDown;
@@ -78,7 +78,7 @@
             フィルタ_ア.Font = new Font("ＭＳ Ｐゴシック", 10F, FontStyle.Regular, GraphicsUnit.Point);
             フィルタ_ア.Location = new Point(10, 40);
             フィルタ_ア.Name = "フィルタ_ア";
-            フィルタ_ア.Size = new Size(50, 30);
+            フィルタ_ア.Size = new Size(53, 30);
             フィルタ_ア.TabIndex = 0;
             フィルタ_ア.Text = "ア";
             フィルタ_ア.Click += フィルタ_ア_Click;
@@ -86,9 +86,9 @@
             // フィルタ_カ
             // 
             フィルタ_カ.Font = new Font("ＭＳ Ｐゴシック", 10F, FontStyle.Regular, GraphicsUnit.Point);
-            フィルタ_カ.Location = new Point(60, 40);
+            フィルタ_カ.Location = new Point(65, 40);
             フィルタ_カ.Name = "フィルタ_カ";
-            フィルタ_カ.Size = new Size(50, 30);
+            フィルタ_カ.Size = new Size(53, 30);
             フィルタ_カ.TabIndex = 0;
             フィルタ_カ.Text = "カ";
             フィルタ_カ.Click += フィルタ_カ_Click;
@@ -96,9 +96,9 @@
             // フィルタ_全て
             // 
             フィルタ_全て.Font = new Font("ＭＳ Ｐゴシック", 10F, FontStyle.Regular, GraphicsUnit.Point);
-            フィルタ_全て.Location = new Point(560, 40);
+            フィルタ_全て.Location = new Point(615, 40);
             フィルタ_全て.Name = "フィルタ_全て";
-            フィルタ_全て.Size = new Size(50, 30);
+            フィルタ_全て.Size = new Size(53, 30);
             フィルタ_全て.TabIndex = 0;
             フィルタ_全て.Text = "全て";
             フィルタ_全て.Click += フィルタ_全て_Click;
@@ -106,9 +106,9 @@
             // フィルタ_abc
             // 
             フィルタ_abc.Font = new Font("ＭＳ Ｐゴシック", 10F, FontStyle.Regular, GraphicsUnit.Point);
-            フィルタ_abc.Location = new Point(510, 40);
+            フィルタ_abc.Location = new Point(560, 40);
             フィルタ_abc.Name = "フィルタ_abc";
-            フィルタ_abc.Size = new Size(50, 30);
+            フィルタ_abc.Size = new Size(53, 30);
             フィルタ_abc.TabIndex = 0;
             フィルタ_abc.Text = "abc";
             フィルタ_abc.Click += フィルタ_abc_Click;
@@ -116,9 +116,9 @@
             // フィルタ_ワ
             // 
             フィルタ_ワ.Font = new Font("ＭＳ Ｐゴシック", 10F, FontStyle.Regular, GraphicsUnit.Point);
-            フィルタ_ワ.Location = new Point(460, 40);
+            フィルタ_ワ.Location = new Point(505, 40);
             フィルタ_ワ.Name = "フィルタ_ワ";
-            フィルタ_ワ.Size = new Size(50, 30);
+            フィルタ_ワ.Size = new Size(53, 30);
             フィルタ_ワ.TabIndex = 0;
             フィルタ_ワ.Text = "ワ";
             フィルタ_ワ.Click += フィルタ_ワ_Click;
@@ -126,9 +126,9 @@
             // フィルタ_ラ
             // 
             フィルタ_ラ.Font = new Font("ＭＳ Ｐゴシック", 10F, FontStyle.Regular, GraphicsUnit.Point);
-            フィルタ_ラ.Location = new Point(410, 40);
+            フィルタ_ラ.Location = new Point(450, 40);
             フィルタ_ラ.Name = "フィルタ_ラ";
-            フィルタ_ラ.Size = new Size(50, 30);
+            フィルタ_ラ.Size = new Size(53, 30);
             フィルタ_ラ.TabIndex = 0;
             フィルタ_ラ.Text = "ラ";
             フィルタ_ラ.Click += フィルタ_ラ_Click;
@@ -136,9 +136,9 @@
             // フィルタ_ヤ
             // 
             フィルタ_ヤ.Font = new Font("ＭＳ Ｐゴシック", 10F, FontStyle.Regular, GraphicsUnit.Point);
-            フィルタ_ヤ.Location = new Point(360, 40);
+            フィルタ_ヤ.Location = new Point(395, 40);
             フィルタ_ヤ.Name = "フィルタ_ヤ";
-            フィルタ_ヤ.Size = new Size(50, 30);
+            フィルタ_ヤ.Size = new Size(53, 30);
             フィルタ_ヤ.TabIndex = 0;
             フィルタ_ヤ.Text = "ヤ";
             フィルタ_ヤ.Click += フィルタ_ヤ_Click;
@@ -146,9 +146,9 @@
             // フィルタ_マ
             // 
             フィルタ_マ.Font = new Font("ＭＳ Ｐゴシック", 10F, FontStyle.Regular, GraphicsUnit.Point);
-            フィルタ_マ.Location = new Point(310, 40);
+            フィルタ_マ.Location = new Point(340, 40);
             フィルタ_マ.Name = "フィルタ_マ";
-            フィルタ_マ.Size = new Size(50, 30);
+            フィルタ_マ.Size = new Size(53, 30);
             フィルタ_マ.TabIndex = 0;
             フィルタ_マ.Text = "マ";
             フィルタ_マ.Click += フィルタ_マ_Click;
@@ -156,9 +156,9 @@
             // フィルタ_ハ
             // 
             フィルタ_ハ.Font = new Font("ＭＳ Ｐゴシック", 10F, FontStyle.Regular, GraphicsUnit.Point);
-            フィルタ_ハ.Location = new Point(260, 40);
+            フィルタ_ハ.Location = new Point(285, 40);
             フィルタ_ハ.Name = "フィルタ_ハ";
-            フィルタ_ハ.Size = new Size(50, 30);
+            フィルタ_ハ.Size = new Size(53, 30);
             フィルタ_ハ.TabIndex = 0;
             フィルタ_ハ.Text = "ハ";
             フィルタ_ハ.Click += フィルタ_ハ_Click;
@@ -166,9 +166,9 @@
             // フィルタ_ナ
             // 
             フィルタ_ナ.Font = new Font("ＭＳ Ｐゴシック", 10F, FontStyle.Regular, GraphicsUnit.Point);
-            フィルタ_ナ.Location = new Point(210, 40);
+            フィルタ_ナ.Location = new Point(230, 40);
             フィルタ_ナ.Name = "フィルタ_ナ";
-            フィルタ_ナ.Size = new Size(50, 30);
+            フィルタ_ナ.Size = new Size(53, 30);
             フィルタ_ナ.TabIndex = 0;
             フィルタ_ナ.Text = "ナ";
             フィルタ_ナ.Click += フィルタ_ナ_Click;
@@ -176,9 +176,9 @@
             // フィルタ_タ
             // 
             フィルタ_タ.Font = new Font("ＭＳ Ｐゴシック", 10F, FontStyle.Regular, GraphicsUnit.Point);
-            フィルタ_タ.Location = new Point(160, 40);
+            フィルタ_タ.Location = new Point(175, 40);
             フィルタ_タ.Name = "フィルタ_タ";
-            フィルタ_タ.Size = new Size(50, 30);
+            フィルタ_タ.Size = new Size(53, 30);
             フィルタ_タ.TabIndex = 0;
             フィルタ_タ.Text = "タ";
             フィルタ_タ.Click += フィルタ_タ_Click;
@@ -186,9 +186,9 @@
             // フィルタ_サ
             // 
             フィルタ_サ.Font = new Font("ＭＳ Ｐゴシック", 10F, FontStyle.Regular, GraphicsUnit.Point);
-            フィルタ_サ.Location = new Point(110, 40);
+            フィルタ_サ.Location = new Point(120, 40);
             フィルタ_サ.Name = "フィルタ_サ";
-            フィルタ_サ.Size = new Size(50, 30);
+            フィルタ_サ.Size = new Size(53, 30);
             フィルタ_サ.TabIndex = 0;
             フィルタ_サ.Text = "サ";
             フィルタ_サ.Click += フィルタ_サ_Click;
@@ -196,7 +196,7 @@
             // キャンセルボタン
             // 
             キャンセルボタン.Font = new Font("ＭＳ Ｐゴシック", 10F, FontStyle.Regular, GraphicsUnit.Point);
-            キャンセルボタン.Location = new Point(490, 10);
+            キャンセルボタン.Location = new Point(549, 10);
             キャンセルボタン.Name = "キャンセルボタン";
             キャンセルボタン.Size = new Size(119, 24);
             キャンセルボタン.TabIndex = 1;
@@ -217,7 +217,7 @@
             // 
             検索ボタン.Enabled = false;
             検索ボタン.Font = new Font("ＭＳ Ｐゴシック", 10F, FontStyle.Regular, GraphicsUnit.Point);
-            検索ボタン.Location = new Point(370, 10);
+            検索ボタン.Location = new Point(428, 10);
             検索ボタン.Name = "検索ボタン";
             検索ボタン.Size = new Size(119, 24);
             検索ボタン.TabIndex = 3;
@@ -230,7 +230,7 @@
             statusStrip1.Location = new Point(0, 633);
             statusStrip1.Name = "statusStrip1";
             statusStrip1.Padding = new Padding(1, 0, 19, 0);
-            statusStrip1.Size = new Size(672, 22);
+            statusStrip1.Size = new Size(786, 22);
             statusStrip1.TabIndex = 111;
             statusStrip1.Text = "statusStrip1";
             // 
@@ -247,7 +247,7 @@
             // 
             // F_検索
             // 
-            ClientSize = new Size(672, 655);
+            ClientSize = new Size(786, 655);
             Controls.Add(statusStrip1);
             Controls.Add(検索ボタン);
             Controls.Add(label1);
