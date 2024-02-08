6ebf32d574fb5e9d9aa92841bf37929970f07b15
Kinoshita Daichi <kinoshita.d@hbm-web.co.jp>
2024-02-08 17:35:08 +0900

    F_メーカー管理_抽出 修正

--- Form/F_メーカー管理_抽出.Designer.cs
+++ Form/F_メーカー管理_抽出.Designer.cs
@@ -58,11 +58,11 @@
             label9.Font = new Font("BIZ UDゴシック", 9F, FontStyle.Regular, GraphicsUnit.Point);
             label9.ForeColor = SystemColors.ActiveCaptionText;
             label9.ImageAlign = ContentAlignment.MiddleLeft;
-            label9.Location = new Point(24, 148);
+            label9.Location = new Point(22, 115);
             label9.Margin = new Padding(0);
             label9.Name = "label9";
-            label9.Size = new Size(85, 17);
-            label9.TabIndex = 8;
+            label9.Size = new Size(85, 20);
+            label9.TabIndex = 10;
             label9.Text = "更新者名(&N)";
             label9.TextAlign = ContentAlignment.MiddleLeft;
             // 
@@ -73,10 +73,10 @@
             label7.Font = new Font("BIZ UDゴシック", 9F, FontStyle.Regular, GraphicsUnit.Point);
             label7.ForeColor = SystemColors.ActiveCaptionText;
             label7.ImageAlign = ContentAlignment.MiddleLeft;
-            label7.Location = new Point(24, 58);
+            label7.Location = new Point(22, 43);
             label7.Margin = new Padding(0);
             label7.Name = "label7";
-            label7.Size = new Size(85, 17);
+            label7.Size = new Size(85, 20);
             label7.TabIndex = 3;
             label7.Text = "担当者名(&C)";
             label7.TextAlign = ContentAlignment.MiddleLeft;
@@ -86,11 +86,11 @@
             担当者名.BackColor = Color.White;
             担当者名.Font = new Font("BIZ UDゴシック", 9.75F, FontStyle.Regular, GraphicsUnit.Point);
             担当者名.ImeMode = ImeMode.Hiragana;
-            担当者名.Location = new Point(175, 53);
+            担当者名.Location = new Point(175, 43);
             担当者名.Margin = new Padding(3, 2, 3, 2);
             担当者名.Name = "担当者名";
-            担当者名.Size = new Size(413, 20);
-            担当者名.TabIndex = 2;
+            担当者名.Size = new Size(310, 20);
+            担当者名.TabIndex = 4;
             // 
             // メーカー名
             // 
@@ -100,8 +100,8 @@
             メーカー名.Location = new Point(175, 20);
             メーカー名.Margin = new Padding(3, 2, 3, 2);
             メーカー名.Name = "メーカー名";
-            メーカー名.Size = new Size(413, 20);
-            メーカー名.TabIndex = 1;
+            メーカー名.Size = new Size(310, 20);
+            メーカー名.TabIndex = 2;
             // 
             // label5
             // 
@@ -110,10 +110,10 @@
             label5.Font = new Font("BIZ UDゴシック", 9F, FontStyle.Regular, GraphicsUnit.Point);
             label5.ForeColor = SystemColors.ActiveCaptionText;
             label5.ImageAlign = ContentAlignment.MiddleLeft;
-            label5.Location = new Point(27, 23);
+            label5.Location = new Point(22, 20);
             label5.Margin = new Padding(0);
             label5.Name = "label5";
-            label5.Size = new Size(87, 17);
+            label5.Size = new Size(87, 20);
             label5.TabIndex = 1;
             label5.Text = "メーカー名(&M)";
             label5.TextAlign = ContentAlignment.MiddleLeft;
@@ -125,11 +125,11 @@
             label1.Font = new Font("BIZ UDゴシック", 9F, FontStyle.Regular, GraphicsUnit.Point);
             label1.ForeColor = SystemColors.ActiveCaptionText;
             label1.ImageAlign = ContentAlignment.MiddleLeft;
-            label1.Location = new Point(24, 117);
+            label1.Location = new Point(22, 92);
             label1.Margin = new Padding(0);
             label1.Name = "label1";
-            label1.Size = new Size(85, 17);
-            label1.TabIndex = 5;
+            label1.Size = new Size(85, 20);
+            label1.TabIndex = 7;
             label1.Text = "更新日(&U)";
             label1.TextAlign = ContentAlignment.MiddleLeft;
             // 
@@ -138,11 +138,11 @@
             更新日開始.BackColor = Color.White;
             更新日開始.Font = new Font("BIZ UDゴシック", 9.75F, FontStyle.Regular, GraphicsUnit.Point);
             更新日開始.ImeMode = ImeMode.Disable;
-            更新日開始.Location = new Point(175, 119);
+            更新日開始.Location = new Point(175, 91);
             更新日開始.Margin = new Padding(3, 2, 3, 2);
             更新日開始.Name = "更新日開始";
-            更新日開始.Size = new Size(160, 20);
-            更新日開始.TabIndex = 4;
+            更新日開始.Size = new Size(112, 20);
+            更新日開始.TabIndex = 8;
             // 
             // label2
             // 
@@ -151,7 +151,7 @@
             label2.Font = new Font("BIZ UDゴシック", 9F, FontStyle.Regular, GraphicsUnit.Point);
             label2.ForeColor = SystemColors.ActiveCaptionText;
             label2.ImageAlign = ContentAlignment.MiddleLeft;
-            label2.Location = new Point(370, 121);
+            label2.Location = new Point(322, 93);
             label2.Margin = new Padding(0);
             label2.Name = "label2";
             label2.Size = new Size(27, 16);
@@ -164,21 +164,21 @@
             更新日終了.BackColor = Color.White;
             更新日終了.Font = new Font("BIZ UDゴシック", 9.75F, FontStyle.Regular, GraphicsUnit.Point);
             更新日終了.ImeMode = ImeMode.Disable;
-            更新日終了.Location = new Point(397, 120);
+            更新日終了.Location = new Point(349, 92);
             更新日終了.Margin = new Padding(3, 2, 3, 2);
             更新日終了.Name = "更新日終了";
-            更新日終了.Size = new Size(160, 20);
-            更新日終了.TabIndex = 5;
+            更新日終了.Size = new Size(112, 20);
+            更新日終了.TabIndex = 9;
             // 
             // Deleted
             // 
             Deleted.Controls.Add(DeletedButton3);
             Deleted.Controls.Add(DeletedButton2);
             Deleted.Controls.Add(DeletedButton1);
-            Deleted.Location = new Point(39, 313);
+            Deleted.Location = new Point(22, 218);
             Deleted.Name = "Deleted";
-            Deleted.Size = new Size(525, 63);
-            Deleted.TabIndex = 207;
+            Deleted.Size = new Size(463, 63);
+            Deleted.TabIndex = 12;
             Deleted.TabStop = false;
             Deleted.Text = "削除指定(&D)";
             // 
@@ -217,20 +217,20 @@
             // 
             // 抽出ボタン
             // 
-            抽出ボタン.Location = new Point(263, 406);
+            抽出ボタン.Location = new Point(281, 296);
             抽出ボタン.Name = "抽出ボタン";
-            抽出ボタン.Size = new Size(114, 23);
-            抽出ボタン.TabIndex = 208;
+            抽出ボタン.Size = new Size(100, 24);
+            抽出ボタン.TabIndex = 13;
             抽出ボタン.Text = "抽出(&O)";
             抽出ボタン.UseVisualStyleBackColor = true;
             抽出ボタン.Click += 抽出ボタン_Click;
             // 
             // キャンセルボタン
             // 
-            キャンセルボタン.Location = new Point(386, 406);
+            キャンセルボタン.Location = new Point(386, 296);
             キャンセルボタン.Name = "キャンセルボタン";
-            キャンセルボタン.Size = new Size(114, 23);
-            キャンセルボタン.TabIndex = 209;
+            キャンセルボタン.Size = new Size(100, 24);
+            キャンセルボタン.TabIndex = 14;
             キャンセルボタン.Text = "キャンセル(&X)";
             キャンセルボタン.UseVisualStyleBackColor = true;
             キャンセルボタン.MouseClick += キャンセルボタン_MouseClick;
@@ -242,11 +242,11 @@
             label3.Font = new Font("BIZ UDゴシック", 9F, FontStyle.Regular, GraphicsUnit.Point);
             label3.ForeColor = SystemColors.ActiveCaptionText;
             label3.ImageAlign = ContentAlignment.MiddleLeft;
-            label3.Location = new Point(24, 90);
+            label3.Location = new Point(22, 66);
             label3.Margin = new Padding(0);
             label3.Name = "label3";
-            label3.Size = new Size(151, 17);
-            label3.TabIndex = 210;
+            label3.Size = new Size(151, 20);
+            label3.TabIndex = 5;
             label3.Text = "担当者メールアドレス(&A)";
             label3.TextAlign = ContentAlignment.MiddleLeft;
             // 
@@ -255,28 +255,28 @@
             担当者メールアドレス.BackColor = Color.White;
             担当者メールアドレス.Font = new Font("BIZ UDゴシック", 9.75F, FontStyle.Regular, GraphicsUnit.Point);
             担当者メールアドレス.ImeMode = ImeMode.Off;
-            担当者メールアドレス.Location = new Point(175, 88);
+            担当者メールアドレス.Location = new Point(175, 67);
             担当者メールアドレス.Margin = new Padding(3, 2, 3, 2);
             担当者メールアドレス.Name = "担当者メールアドレス";
-            担当者メールアドレス.Size = new Size(413, 20);
-            担当者メールアドレス.TabIndex = 3;
+            担当者メールアドレス.Size = new Size(310, 20);
+            担当者メールアドレス.TabIndex = 6;
             // 
             // 更新者名
             // 
             更新者名.BackColor = Color.White;
             更新者名.Font = new Font("BIZ UDゴシック", 9.75F, FontStyle.Regular, GraphicsUnit.Point);
-            更新者名.Location = new Point(175, 153);
+            更新者名.Location = new Point(175, 115);
             更新者名.Margin = new Padding(3, 2, 3, 2);
             更新者名.Name = "更新者名";
-            更新者名.Size = new Size(413, 20);
-            更新者名.TabIndex = 6;
+            更新者名.Size = new Size(310, 20);
+            更新者名.TabIndex = 11;
             // 
             // 更新日終了選択
             // 
-            更新日終了選択.Location = new Point(559, 120);
+            更新日終了選択.Location = new Point(463, 91);
             更新日終了選択.Margin = new Padding(4);
             更新日終了選択.Name = "更新日終了選択";
-            更新日終了選択.Size = new Size(29, 23);
+            更新日終了選択.Size = new Size(22, 22);
             更新日終了選択.TabIndex = 211;
             更新日終了選択.Text = "▼";
             更新日終了選択.UseVisualStyleBackColor = true;
@@ -284,10 +284,10 @@
             // 
             // 更新日開始選択
             // 
-            更新日開始選択.Location = new Point(337, 120);
+            更新日開始選択.Location = new Point(289, 90);
             更新日開始選択.Margin = new Padding(4);
             更新日開始選択.Name = "更新日開始選択";
-            更新日開始選択.Size = new Size(29, 23);
+            更新日開始選択.Size = new Size(22, 22);
             更新日開始選択.TabIndex = 212;
             更新日開始選択.Text = "▼";
             更新日開始選択.UseVisualStyleBackColor = true;
@@ -297,7 +297,7 @@
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(612, 438);
+            ClientSize = new Size(498, 333);
             Controls.Add(更新日開始選択);
             Controls.Add(更新日終了選択);
             Controls.Add(更新者名);
@@ -318,7 +318,7 @@
             KeyPreview = true;
             Name = "F_メーカー管理_抽出";
             StartPosition = FormStartPosition.CenterScreen;
-            Text = "メーカー管理_抽出";
+            Text = "メーカー管理 - 抽出";
             FormClosing += F_メーカー管理_抽出_FormClosing;
             Load += Form_Load;
             KeyDown += F_メーカー管理_抽出_KeyDown;
